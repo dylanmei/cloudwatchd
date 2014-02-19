5f99883faac187e761b5559a55a7470d03643b78
dylanmei <dylanmei@gmail.com>
2014-02-19 08:38:12 -0800

    Initial commit

---
+++ backends/syslog.js
@@ -0,0 +1,50 @@
+
+var bunyan = require('bunyan'),
+    syslog = require('bunyan-syslog'),
+    _      = require('underscore')
+
+var log 
+
+exports.init = function(config) {
+  config = _.defaults(config.syslog || {}, {
+    type:     'udp',
+    host:     '127.0.0.1',
+    port:     514,
+    facility: 'local0',
+  })
+
+  log = bunyan.createLogger({
+    name: 'cloudwatch',
+    streams: [{
+      level: 'info',
+      type: 'raw',
+      stream: syslog.createBunyanStream({
+        type: config.type,
+        facility: syslog[config.facility],
+        host: config.host,
+        port: config.port
+      })
+    }]
+  })
+
+  return log ? 1 : 0
+}
+
+exports.send = function(time, metric, value) {
+  console.log('[syslog]', time, metric.MetricName, value)
+
+  var message = {
+    time: time.toISOString(),
+    Namespace:  metric.Namespace,
+    MetricName: metric.MetricName,
+    Unit:       metric.Unit,
+    Statistic:  metric.Statistic,
+    Value:      value,
+  }
+
+  _.each(metric.Dimensions, function(d) {
+    message[d.Name] = d.Value
+  })
+
+  log.info(message)
+}
---
+++ cloudwatch.js
@@ -0,0 +1,87 @@
+
+var AWS    = require('aws-sdk'),
+    bunyan = require('bunyan'),
+    syslog = require('bunyan-syslog'),
+    fs     = require('fs'),
+    _      = require('underscore')
+
+var timestamp = 0
+var config = _.defaults(eval('c='+fs.readFileSync(process.argv[2])), {
+  metrics:  [],
+  backends: [],
+  interval: 60,
+})
+
+AWS.config.update(config)
+AWS.config.apiVersions = {
+  cloudwatch: '2010-08-01',
+}
+var cloudwatch = new AWS.CloudWatch()
+
+var backends = _.map(config.backends, function(spec, i) {
+  backend = require(spec)
+  if (!backend.init(config)) {
+    console.log('[cloudwatch] failed to load backend', spec)
+    process.exit(1)
+  }
+  return backend
+})
+
+function run() {
+  var now = new Date()
+  var start = timestamp
+    ? new Date(timestamp)
+    : new Date(now.getTime() - (config.interval * 1000))
+  timestamp = now.getTime()
+
+  _.each(config.metrics, function(m) {
+    run_metric(m, start, now)
+  })
+}
+
+function run_metric(metric, start, end) {
+  var params = {
+    Namespace: metric.Namespace,
+    MetricName: metric.MetricName,
+    StartTime: start.toISOString(),
+    EndTime: end.toISOString(),
+    Statistics: [metric.Statistic],
+    Dimensions: metric.Dimensions || [],
+    Period: '60',
+    Unit: metric.Unit
+  }
+
+  console.log(params)
+
+  cloudwatch.getMetricStatistics(params, function(error, response) {
+    if (error) {
+      return console.error("[cloudwatch] error:", error)
+    }
+
+    console.log(response)
+
+    _.each(response.Datapoints, function(dp) {
+      send_datapoint_to_backends(metric, dp)
+    })
+  })
+}
+
+function send_datapoint_to_backends(metric, dp) {
+  var time = new Date(dp.Timestamp)
+  var value = dp[metric.Statistic]
+
+  _.each(backends, function(b) {
+    send_datapoint_to_backend(b, time, metric, value)
+  })
+}
+
+function send_datapoint_to_backend(backend, time, metric, value) {
+  backend.send(time, metric, value)
+}
+
+run() || setInterval(run, config.interval * 1000)
+
+process.title = 'cloudwatchd'
+process.on('exit', function() {
+  console.log('exiting')
+})
